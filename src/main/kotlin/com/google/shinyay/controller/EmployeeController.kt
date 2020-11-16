416196c5300870acc34e2d6780a6acc7164d7f91
shinyay <shinya.com@gmail.com>
2020-11-16 23:02:02 +0900

    Add info message

--- src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
+++ src/main/kotlin/com/google/shinyay/controller/EmployeeController.kt
@@ -1,6 +1,7 @@
 package com.google.shinyay.controller
 
 import com.google.shinyay.entity.Employee
+import com.google.shinyay.logger
 import com.google.shinyay.repository.EmployeeRepository
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,6 +13,7 @@ class EmployeeController(val repository: EmployeeRepository) {
 
     @GetMapping("")
     fun getAllEmployees(): MutableIterable<Employee> {
+        logger.info("-----> getAllEmployees")
         return repository.findAll()
     }
 }

518e48426262f66c3a68c2b0af7c05922acc4923
shinyay <shinya.com@gmail.com>
2020-11-19 13:51:40 +0900

    Add default access controller class

--- src/main/kotlin/com/google/shinyay/controller/HelloController.kt
+++ src/main/kotlin/com/google/shinyay/controller/HelloController.kt
@@ -3,13 +3,15 @@ package com.google.shinyay.controller
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.time.ZoneId
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
 
 @RestController
 @RequestMapping("/")
 class HelloController {
 
     @GetMapping
-    fun defaultAccess() {
-        
-    }
+    fun defaultAccess() =
+            "Hello at ${ZonedDateTime.now(ZoneId.of("Japan")).format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))}"
 }
